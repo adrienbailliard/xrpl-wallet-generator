f2fbfee9e30d343e89cf1ca28c15a5d72ec947b0
Adrien Bailliard <unknown>
2025-03-20 19:18:03 +0100

    Upload to github

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Wallet } from "xrpl";
+import qrcode from 'qrcode-terminal';
+
+
+function generateWallet(): void
+{
+	console.log("Here is your new wallet!\n");
+
+	const wallet: Wallet = Wallet.generate();
+
+	console.log("Adress: " + wallet.address);
+	qrcode.generate(wallet.address, {small: true});
+
+	console.log("\nSeed: " + wallet.seed);
+	qrcode.generate(wallet.seed!, {small: true});
+
+	process.stdout.write("\nPress [Enter] to get a new one...");
+}
+
+
+process.stdout.write('\x1B[2J\x1B[3J\x1B[H');
+process.stdout.write('\x1b]0;XRPL Wallet Generator\x07');
+console.log("XRPL Wallet Generator,\nby Adrien Bailliard\n\n");
+
+
+generateWallet();
+
+process.stdin.on('data', () =>
+{
+	process.stdout.write('\x1b[1A');
+	process.stdout.write('\x1b[2K');
+	generateWallet();
+});
